1c62683268cffb1aae78fbc22b0a08fcbabe501c
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2025-04-19 22:55:34 +0200

    Update documentrw.cpp

--- src/core/Private/documentrw.cpp
+++ src/core/Private/documentrw.cpp
@@ -34,70 +34,68 @@
 #include "canvas.h"
 #include "appsupport.h"
 
-void Document::writeBookmarked(eWriteStream &dst) const {
+void Document::writeBookmarked(eWriteStream &dst) const
+{
     dst << fColors.count();
-    for(const auto &col : fColors) {
-        dst << col;
-    }
+    for (const auto &col : fColors) { dst << col; }
 
     dst << fBrushes.count();
-    for(const auto &brush : fBrushes) {
-        dst << brush;
-    }
+    for (const auto &brush : fBrushes) { dst << brush; }
 }
 
-void Document::writeScenes(eWriteStream& dst) const {
+void Document::writeScenes(eWriteStream& dst) const
+{
     writeBookmarked(dst);
     dst.writeCheckpoint();
 
     const int nScenes = fScenes.count();
     dst.write(&nScenes, sizeof(int));
-    for(const auto &scene : fScenes) {
+    for (const auto &scene : fScenes) {
         scene->writeBoundingBox(dst);
         dst.writeCheckpoint();
     }
 }
 
-void Document::readBookmarked(eReadStream &src) {
+void Document::readBookmarked(eReadStream &src)
+{
     int nCol; src >> nCol;
-    for(int i = 0; i < nCol; i++) {
+    for (int i = 0; i < nCol; i++) {
         QColor col; src >> col;
         addBookmarkColor(col);
     }
 
     int nBrush; src >> nBrush;
-    for(int i = 0; i < nBrush; i++) {
+    for (int i = 0; i < nBrush; i++) {
         SimpleBrushWrapper* brush; src >> brush;
-        if(brush) addBookmarkBrush(brush);
+        if (brush) { addBookmarkBrush(brush); }
     }
 }
 
-void Document::readGradients(eReadStream& src) {
+void Document::readGradients(eReadStream& src)
+{
     int nGrads; src >> nGrads;
-    for(int i = 0; i < nGrads; i++) {
+    for (int i = 0; i < nGrads; i++) {
         enve::make_shared<SceneBoundGradient>(nullptr)->read(src);
     }
 }
 
-void Document::readScenes(eReadStream& src) {
-    if(src.evFileVersion() > 1) {
+void Document::readScenes(eReadStream& src)
+{
+    if (src.evFileVersion() > 1) {
         readBookmarked(src);
         src.readCheckpoint("Error reading bookmarks");
     }
-    if(src.evFileVersion() <= 5) {
+    if (src.evFileVersion() <= 5) {
         readGradients(src);
         src.readCheckpoint("Error reading gradients");
     }
 
     int nScenes;
     src.read(&nScenes, sizeof(int));
-    for(int i = 0; i < nScenes; i++) {
+    for (int i = 0; i < nScenes; i++) {
         Canvas* scene;
-        if(src.evFileVersion() < EvFormat::betterSWTAbsReadWrite) {
-            scene = createNewScene();
-        } else {
-            scene = fScenes.at(fScenes.count() - nScenes + i).get();
-        }
+        if (src.evFileVersion() < EvFormat::betterSWTAbsReadWrite) { scene = createNewScene(); }
+        else { scene = fScenes.at(fScenes.count() - nScenes + i).get(); }
         const auto block = scene->blockUndoRedo();
         scene->readBoundingBox(src);
         src.readCheckpoint("Error reading scene");
@@ -106,27 +104,29 @@ void Document::readScenes(eReadStream& src) {
     SimpleTask::sProcessAll();
 }
 
-void Document::writeDoxumentXML(QDomDocument& doc) const {
+void Document::writeDoxumentXML(QDomDocument& doc) const
+{
     auto document = doc.createElement("Document");
     document.setAttribute("format-version", XevFormat::version);
 
     auto bColors = doc.createElement("ColorBookmarks");
-    for(const auto &col : fColors) {
+    for (const auto &col : fColors) {
         auto color = doc.createElement("Color");
         color.setAttribute("name", col.name());
         bColors.appendChild(color);
     }
     document.appendChild(bColors);
 
-    auto bBrushes = doc.createElement("BrushBookmarks");
+    // REMOVE
+    /*auto bBrushes = doc.createElement("BrushBookmarks");
     for(const auto &b : fBrushes) {
         const auto brush = Friction::Core::XevExportHelpers::brushToElement(b, doc);
         bBrushes.appendChild(brush);
     }
-    document.appendChild(bBrushes);
+    document.appendChild(bBrushes);*/
 
     auto scenes = doc.createElement("Scenes");
-    for(const auto &s : fScenes) {
+    for (const auto &s : fScenes) {
         auto scene = doc.createElement("Scene");
         const qreal resolution = s->getResolution();
         scene.setAttribute("resolution", QString::number(resolution));
@@ -148,16 +148,18 @@ void Document::writeDoxumentXML(QDomDocument& doc) const {
 }
 
 void Document::writeScenesXML(const std::shared_ptr<Friction::Core::XmlZipFileSaver>& xevFileSaver,
-                              const Friction::Core::RuntimeIdToWriteId& objListIdConv) const {
+                              const Friction::Core::RuntimeIdToWriteId& objListIdConv) const
+{
     int id = 0;
-    for(const auto &s : fScenes) {
+    for (const auto &s : fScenes) {
         const QString path = "scenes/" + QString::number(id++) + "/";
         s->writeBoxOrSoundXML(xevFileSaver, objListIdConv, path);
     }
 }
 
 void Document::writeXML(const std::shared_ptr<Friction::Core::XmlZipFileSaver>& xevFileSaver,
-                        const Friction::Core::RuntimeIdToWriteId& objListIdConv) const {
+                        const Friction::Core::RuntimeIdToWriteId& objListIdConv) const
+{
     auto& fileSaver = xevFileSaver->fileSaver();
     fileSaver.processText("document.xml", [&](QTextStream& stream) {
         QDomDocument document;
@@ -168,7 +170,8 @@ void Document::writeXML(const std::shared_ptr<Friction::Core::XmlZipFileSaver>& 
 }
 
 void Document::readDocumentXML(Friction::Core::ZipFileLoader& fileLoader,
-                               QList<Canvas*>& scenes) {
+                               QList<Canvas*>& scenes)
+{
     fileLoader.process("document.xml", [&](QIODevice* const src) {
         QDomDocument document;
         document.setContent(src);
@@ -177,25 +180,27 @@ void Document::readDocumentXML(Friction::Core::ZipFileLoader& fileLoader,
 }
 
 void Document::readDocumentXML(const QDomDocument& doc,
-                               QList<Canvas*>& scenes) {
+                               QList<Canvas*>& scenes)
+{
     const auto document = doc.firstChildElement("Document");
     const QString versionStr = document.attribute("format-version", "");
-    if(versionStr.isEmpty()) RuntimeThrow("No format version specified");
+    if (versionStr.isEmpty()) { RuntimeThrow("No format version specified"); }
 //    const int version = Friction::Core::XmlExportHelpers::stringToInt(versionStr);
 
     auto bColors = document.firstChildElement("ColorBookmarks");
     const auto colors = bColors.elementsByTagName("Color");
     const int nColors = colors.count();
-    for(int i = 0; i < nColors; i++) {
+    for (int i = 0; i < nColors; i++) {
         const auto color = colors.at(i);
-        if(!color.isElement()) continue;
+        if (!color.isElement()) { continue; }
         const auto colorEle = color.toElement();
         const QString name = colorEle.attribute("name");
-        if(name.isEmpty()) continue;
+        if (name.isEmpty()) { continue; }
         addBookmarkColor(QColor(name));
     }
 
-    auto bBrushes = document.firstChildElement("BrushBookmarks");
+    // REMOVE
+    /*auto bBrushes = document.firstChildElement("BrushBookmarks");
     const auto brushes = bBrushes.elementsByTagName("Brush");
     const int nBrushes = brushes.count();
     for(int i = 0; i < nBrushes; i++) {
@@ -204,14 +209,14 @@ void Document::readDocumentXML(const QDomDocument& doc,
         const auto brushEle = brush.toElement();
         const auto brushPtr = Friction::Core::XevExportHelpers::brushFromElement(brushEle);
         if(brushPtr) addBookmarkBrush(brushPtr);
-    }
+    }*/
 
     auto scenesE = document.firstChildElement("Scenes");
     const auto sceneEles = scenesE.elementsByTagName("Scene");
     const int nScenes = sceneEles.count();
-    for(int i = 0; i < nScenes; i++) {
+    for (int i = 0; i < nScenes; i++) {
         const auto sceneNode = sceneEles.at(i);
-        if(!sceneNode.isElement()) continue;
+        if (!sceneNode.isElement()) { continue; }
         const auto sceneEle = sceneNode.toElement();
 
         const auto resStr = sceneEle.attribute("resolution");
@@ -223,7 +228,7 @@ void Document::readDocumentXML(const QDomDocument& doc,
         const bool clip = sceneEle.attribute("clip") == "true";
         const auto rangeStr = sceneEle.attribute("frameRange", "0 200");
         const auto rangeStrs = rangeStr.split(' ', Qt::SkipEmptyParts);
-        if(rangeStrs.count() != 2) RuntimeThrow("Invalid frame range " + rangeStr);
+        if (rangeStrs.count() != 2) { RuntimeThrow("Invalid frame range " + rangeStr); }
         const int rangeMin = Friction::Core::XmlExportHelpers::stringToInt(rangeStrs[0]);
         const int rangeMax = Friction::Core::XmlExportHelpers::stringToInt(rangeStrs[1]);
 
@@ -243,9 +248,10 @@ void Document::readDocumentXML(const QDomDocument& doc,
 void Document::readScenesXML(Friction::Core::XmlReadBoxesHandler& boxReadHandler,
                              Friction::Core::ZipFileLoader& fileLoader,
                              const QList<Canvas*>& scenes,
-                             const Friction::Core::RuntimeIdToWriteId& objListIdConv) {
+                             const Friction::Core::RuntimeIdToWriteId& objListIdConv)
+{
     int id = 0;
-    for(const auto& scene : scenes) {
+    for (const auto& scene : scenes) {
         const auto block = scene->blockUndoRedo();
         const QString path = "scenes/" + QString::number(id++) + "/";
         scene->readBoxOrSoundXML(boxReadHandler, fileLoader,
