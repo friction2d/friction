9f0ef659581e080fca88639331eb141225c45807
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2026-06-02 22:15:06 +0200

    Update canvas.cpp
    macOS: invalidate cache on set resolution.
    
    Co-authored-by: Pablo Gil <pgilfernandez@gmail.com>

--- src/core/canvas.cpp
+++ src/core/canvas.cpp
@@ -125,7 +125,11 @@ qreal Canvas::getResolution() const
 
 void Canvas::setResolution(const qreal percent)
 {
+    if (isZero6Dec(mResolution - percent)) { return; }
     mResolution = percent;
+#ifdef Q_OS_MAC
+    invalidateSceneFramesCache();
+#endif
     prp_afterWholeInfluenceRangeChanged();
     updateAllBoxes(UpdateReason::userChange);
 }
